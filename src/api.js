3dda4ac2a6e6ae8a2cff530c54f89d2ecc1597c0
vochithanh89 <61690947+vochithanh89@users.noreply.github.com>
2022-07-24 09:12:28 +0700

    change base url

--- src/api.js
+++ src/api.js
@@ -2,9 +2,9 @@ const express = require('express');
 const cheerio = require('cheerio');
 const axios = require('axios');
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8080;
 
-const baseUrl = 'http://www.nettruyenme.com';
+const baseUrl = 'http://nhattruyenone.com';
 
 const getHtmlData = async (path, options) => {
   const res = await axios.get(baseUrl + path, options);
